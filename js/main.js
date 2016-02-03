794d5a06d0bd6f7191e971f342d8d734689ad216
Jonatan Östling <jonatan.ostling@gmail.com>
2016-02-03 06:03:19 +0100

    went from a specific channel to the search

--- js/main.js
+++ js/main.js
@@ -1,31 +1,19 @@
-var channelName = 'TechGuyWeb';
-var videoResults = 10;
-var vidWidth = 640;
-var vidHeight = 360;
+var videoResults = 1;
+
 
 $(document).ready(function() {
-  $.get(
-    "https://www.googleapis.com/youtube/v3/channels",{
-      part: 'contentDetails',
-      forUsername: channelName,
-      key: 'AIzaSyAEWYyHq1Tf8XR1sTJtC1Skgv8xAWzvF9s'},
-      function(data){
-        console.log(data);
-        $.each(data.items, function(i, item) {
-          console.log(item);
-          var pid = item.contentDetails.relatedPlaylists.uploads;
-          getVids(pid);
-        });
-      }
-  );
+  $('form').on('submit', function(e){
 
-  function getVids(pid) {
-    console.log('Inside getVids function');
+    e.preventDefault();
+    console.log(1);
     $.get(
-      "https://www.googleapis.com/youtube/v3/playlistItems",{
+      "https://www.googleapis.com/youtube/v3/search",{
         part: 'snippet',
+        type: 'video',
+        q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
         maxResults: videoResults,
-        playlistId: pid,
+        order: 'viewCount',
+        publishedAfter: '2016-01-01T00:00:00Z',
         key: 'AIzaSyAEWYyHq1Tf8XR1sTJtC1Skgv8xAWzvF9s'},
         function(data){
           var output;
@@ -33,15 +21,30 @@ $(document).ready(function() {
           $.each(data.items, function(i, item) {
             console.log(item);
             var videoTitle = item.snippet.title;
-            var videoId = item.snippet.resourceId.videoId;
+            var videoId = item.id.videoId;
             console.log(videoId);
-            output = '<li><iframe class="videoBox" src=\"//www.youtube.com/embed/'+videoId+'\"></iframe></li>';
+            output = '<li>'+videoTitle+'<iframe class="videoBox" src=\"//www.youtube.com/embed/'+videoId+'\"></iframe></li>';
 
             //Append to the results list
             $('#results').append(output);
           });
         }
-    );
+      );
 
-  }
-});
+    });
+  });
+  // //First function
+  // $.get(
+  //   "https://www.googleapis.com/youtube/v3/channels",{
+  //     part: 'contentDetails',
+  //     forUsername: channelName,
+  //     key: 'AIzaSyAEWYyHq1Tf8XR1sTJtC1Skgv8xAWzvF9s'},
+  //     function(data){
+  //       console.log(data);
+  //       $.each(data.items, function(i, item) {
+  //         console.log(item);
+  //         var pid = item.contentDetails.relatedPlaylists.uploads;
+  //         getVids(pid);
+  //       });
+  //     }
+  // );

cbf6c41bcc8d0c567de3f621c4653274e9ee1281
Jonatan Östling <jonatan.ostling@gmail.com>
2016-02-03 22:09:19 +0100

    need another ajax request

--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@ var videoResults = 1;
 
 
 $(document).ready(function() {
+  //Grab form, and att clickhandler on submit
   $('form').on('submit', function(e){
-
+    //Prevent form from reloading page
     e.preventDefault();
-    console.log(1);
+
+    //Open request
     $.get(
       "https://www.googleapis.com/youtube/v3/search",{
         part: 'snippet',
@@ -14,24 +16,27 @@ $(document).ready(function() {
         maxResults: videoResults,
         order: 'viewCount',
         key: 'AIzaSyAEWYyHq1Tf8XR1sTJtC1Skgv8xAWzvF9s'},
+        //success
         function(data){
           var output;
           console.log(data);
+          $('#vidResults').html("");
           $.each(data.items, function(i, item) {
             console.log(item);
             var videoTitle = item.snippet.title;
             var videoId = item.id.videoId;
             console.log(videoId);
-            output = '<li>'+videoTitle+'<iframe class="videoBox" src=\"//www.youtube.com/embed/'+videoId+'\"></iframe></li>';
+            output = '<li><p>'+videoTitle+'</p><iframe class="videoBox" src=\"//www.youtube.com/embed/'+videoId+'\"></iframe></li>';
 
             //Append to the results list
-            $('#results').append(output);
+            $('#vidResults').append(output);
           });
         }
       );
 
     });
-  });
+  }); // DOM ready
+
   // //First function
   // $.get(
   //   "https://www.googleapis.com/youtube/v3/channels",{

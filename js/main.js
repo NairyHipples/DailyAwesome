d96c54a1269e9641dcee8c592347e0de0640297d
Jonatan Östling <jonatan.ostling@gmail.com>
2016-02-04 03:19:55 +0100

    Updated some bugs

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 var videoResults = 1;
-var submitter = $('#submit');
-var search = $('#search');
 
 $(document).ready(function() {
   //Grab form, and att clickhandler on submit
@@ -8,18 +6,18 @@ $(document).ready(function() {
     //Prevent form from reloading page
     e.preventDefault();
 
-    submitter.prop("disabled", true);
-    search.attr("disabled", true).val("Searching...");
+    $('#submit').prop("disabled", true);
+    $('#search').attr("disabled", true).val("Searching...");
     //Open request
     $.get(
       "https://www.googleapis.com/youtube/v3/search",{
         part: 'snippet',
         type: 'video',
-        q: encodeURIComponent(search.val()).replace(/%20/g, "+"),
+        q: encodeURIComponent($('#search').val()).replace(/%20/g, "+"),
         maxResults: videoResults,
         order: 'viewCount',
         key: 'AIzaSyAEWYyHq1Tf8XR1sTJtC1Skgv8xAWzvF9s'},
-        //success
+        //success callback
         function(data){
           var output;
           console.log(data);
@@ -33,13 +31,15 @@ $(document).ready(function() {
 
             //Append to the results list
             $('#vidResults').append(output);
+            $('#picResults').hide();
             $('.backgroundText').hide();
             $('.videosTab').show();
             $('#vidResults').show();
+            $('.picturesClick').removeClass('active');
             $('.videosClick').addClass('active');
-            search.prop("disabled", false);
-            submitter.attr("disabled", false).val("Search...");
           });
+          $('#search').prop("disabled", false).val("");
+          $('#submit').attr("disabled", false);
         }
       );
     }); //end video request
@@ -47,12 +47,10 @@ $(document).ready(function() {
     $('form').on('submit', function(e) {
       e.preventDefault();
 
-      submitter.prop("disabled", true);
-      search.attr("disabled", true).val("Searching...");
       //ajax
       var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
       var flickrOptions = {
-        tags: search.val(),
+        tags: $('#search').val(),
         format: "json"
       };
       //ajax callback
@@ -60,14 +58,11 @@ $(document).ready(function() {
         var photoHTML;
         $('#picResults').html("");
         $.each(data.items, function(i, photo) {
-          console.log(photo.media.m);
           photoHTML = '<li><div class="iframebox">'
           photoHTML += '<a href="'+photo.link+'">';
           photoHTML += '<img src="'+photo.media.m+'"/></a></div></li>';
         });
         $('#picResults').append(photoHTML);
-        search.prop("disabled", false);
-        submitter.attr("disabled", false).val("Search...");
       }
       $.getJSON(flickrAPI, flickrOptions, displayPhotos);
     });//end picture request

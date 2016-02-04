838b99db9fd423c62fcbccab82389431cc85b7a4
Jonatan Östling <jonatan.ostling@gmail.com>
2016-02-04 03:00:27 +0100

    added another ajax call for flicker

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var videoResults = 1;
-
+var submitter = $('#submit');
+var search = $('#search');
 
 $(document).ready(function() {
   //Grab form, and att clickhandler on submit
@@ -7,12 +8,14 @@ $(document).ready(function() {
     //Prevent form from reloading page
     e.preventDefault();
 
+    submitter.prop("disabled", true);
+    search.attr("disabled", true).val("Searching...");
     //Open request
     $.get(
       "https://www.googleapis.com/youtube/v3/search",{
         part: 'snippet',
         type: 'video',
-        q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
+        q: encodeURIComponent(search.val()).replace(/%20/g, "+"),
         maxResults: videoResults,
         order: 'viewCount',
         key: 'AIzaSyAEWYyHq1Tf8XR1sTJtC1Skgv8xAWzvF9s'},
@@ -26,14 +29,45 @@ $(document).ready(function() {
             var videoTitle = item.snippet.title;
             var videoId = item.id.videoId;
             console.log(videoId);
-            output = '<li><p><strong>Title: </strong>'+videoTitle+'</p><iframe class="videoBox" src=\"//www.youtube.com/embed/'+videoId+'\"></iframe></li>';
+            output = '<li><p><strong>Title: </strong>'+videoTitle+'</p><div class="iframebox"><iframe class="videoBox" src=\"//www.youtube.com/embed/'+videoId+'\"></iframe></div></li>';
 
             //Append to the results list
             $('#vidResults').append(output);
+            $('.backgroundText').hide();
+            $('.videosTab').show();
+            $('#vidResults').show();
+            $('.videosClick').addClass('active');
+
           });
         }
       );
-    });
+    }); //end video request
+
+    $('form').on('submit', function(e) {
+      e.preventDefault();
+
+      //ajax
+      var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+      var flickrOptions = {
+        tags: search.val(),
+        format: "json"
+      };
+      //ajax callback
+      function displayPhotos(data) {
+        var photoHTML;
+        $('#picResults').html("");
+        $.each(data.items, function(i, photo) {
+          console.log(photo.media.m);
+          photoHTML = '<li><div class="iframebox">'
+          photoHTML += '<a href="'+photo.link+'">';
+          photoHTML += '<img src="'+photo.media.m+'"/></a></div></li>';
+        });
+        $('#picResults').append(photoHTML);
+        search.prop("disabled", false);
+        submitter.attr("disabled", false).val("Search...");
+      }
+      $.getJSON(flickrAPI, flickrOptions, displayPhotos);
+    });//end picture request
 
     $('.videosClick').on('click', function() {
       if ($('.picturesClick').hasClass('active')) {
@@ -42,6 +76,7 @@ $(document).ready(function() {
       $(this).addClass('active');
 
       $('#picResults').hide();
+      $('.backgroundText').hide();
       $('#vidResults').show();
       $('.videosTab').show();
 
@@ -53,6 +88,7 @@ $(document).ready(function() {
       $(this).addClass('active');
 
       $('#vidResults').hide();
+      $('.backgroundText').hide();
       $('#picResults').show();
       $('.picturesTab').show();
     });
